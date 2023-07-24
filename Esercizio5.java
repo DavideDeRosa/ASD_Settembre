2796ef1ac97a45291aeb4961727de6e4f93eae58
davidederosa <davide.derosa@studio.unibo.it>
2023-07-24 18:06:40 +0200

    Creazione file input esercizio5 + extra

--- Esercizio5.java
+++ Esercizio5.java
@@ -3,8 +3,20 @@
  *  Cognome: De Rosa
  *  Matricola: 1054948
  *  Email: davide.derosa@studio.unibo.it
+ * 
+ * Esecuzione:
+ * Per compilare: javac Esercizio5.java
+ * Per eseguire: java Esercizio5 <file input>
+ * 
+ * Considerazioni e richieste extra:
+ * ciao
  */
 
 public class Esercizio5 {
+
     
+
+    public static void main(String[] args) {
+        
+    }
 }
